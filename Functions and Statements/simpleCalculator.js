c4d78a34588799a6df0426efbad691913ffa1c7e
Georgi Nikov <144832142+GeorgNikov@users.noreply.github.com>
2025-03-04 20:27:11 +0200

    Add second solution to simpleCalculator.js

--- Functions and Statements/simpleCalculator.js
+++ Functions and Statements/simpleCalculator.js
@@ -1,16 +1,28 @@
+// function calculate(num1, num2, operator) {
+//     switch (operator) {
+//         case "multiply":
+//             return num1 * num2;
+//         case "divide":
+//             return num1 / num2;
+//         case "add":
+//             return num1 + num2;
+//         case "subtract":
+//             return num1 - num2;
+//         default:
+//             return "Invalid operator";
+//     }
+// }
+
 function calculate(num1, num2, operator) {
-    switch (operator) {
-        case "multiply":
-            return num1 * num2;
-        case "divide":
-            return num1 / num2;
-        case "add":
-            return num1 + num2;
-        case "subtract":
-            return num1 - num2;
-        default:
-            return "Invalid operator";
-    }
+    const operations = {
+        multiply: (a, b) => a * b,
+        divide: (a, b) => a / b,
+        add: (a, b) => a + b,
+        subtract: (a, b) => a - b
+    };
+
+    return operations[operator](num1, num2);
 }
 
-console.log(calculate(5, 5, "subtract"))
+
+console.log(calculate(55, 5, "divide"))
