c70580df1189983d9848a9567544613e04edfeab
Georgi Nikov <144832142+GeorgNikov@users.noreply.github.com>
2025-04-06 18:03:49 +0300

    Add Asynchronous Programming

--- Asynchronous Programming/02.Bus-Schedule/app.js
+++ Asynchronous Programming/02.Bus-Schedule/app.js
@@ -1,11 +1,33 @@
 function solve() {
+    let currentStopId = 'depot';
+    let currentStopName = '';
 
-    function depart() {
-        console.log('Depart TODO...');
+    async function depart() {
+        try {
+            const response = await fetch(`http://localhost:3030/jsonstore/bus/schedule/${currentStopId}`);
+            const data = await response.json();
+
+            if (data && data.name && data.next) {
+                document.querySelector('.info').textContent = `Next stop ${data.name}`;
+                currentStopId = data.next;
+                currentStopName = data.name;
+
+                document.getElementById('depart').disabled = true;
+                document.getElementById('arrive').disabled = false;
+            } else {
+                throw new Error('Invalid data received');
+            }
+        } catch (error) {
+            document.querySelector('.info').textContent = 'Error';
+            document.getElementById('depart').disabled = true;
+            document.getElementById('arrive').disabled = true;
+        }
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        document.querySelector('.info').textContent = `Arriving at ${currentStopName}`;
+        document.getElementById('depart').disabled = false;
+        document.getElementById('arrive').disabled = true;
     }
 
     return {
